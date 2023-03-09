749568dc33cba20accdda3c5f1d047dfc945c491
ASMITA MALLICK <asmitamallick2@gmail.com>
2023-03-09 23:57:31 +0530

    cpp

---
+++ function prototype, cpp 1 day 10.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int sum(int a, int b){
+	int c=a+b;
+	return c;
+
+}
+int main(){
+	int num1, num2;
+	cout<<"enter num1"<<endl;
+	cin>>num1;
+	cout<<"enter num2"<<endl;
+	cin>>num2;
+	cout<<"the sum is"<<sum(num1, num2);
+	return 0;
+}
